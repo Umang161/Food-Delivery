0507422fb35ba4e8ae156af11ffd256201a5a0d3
Umang Agarwal <umang@Umangs-MacBook-Air.local>
2023-09-04 00:22:23 +0530

    Adds feature to display top-rated restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,25 @@
 import {RestaurantCard} from "./RestaurantCard"
 import {restaurantsList} from "../utils/mock-data"
+import { useState } from "react"
 
 export const Body = () => {
+    let [restaurantData, setRestaurantData] = useState(restaurantsList);
+
     return(
         <div className="body">
             <div className="search-bar"> Search Bar Here.</div>
+            <div className="filter">
+                <button className="topRated" onClick={()=>
+                 {
+                  const filteredList = restaurantsList.filter((restaurant) => { return restaurant.info.avgRating > 4.5});
+                  console.log(filteredList);
+                  setRestaurantData(filteredList);
+                 }
+                }>Top-Rated</button>    
+            </div>
             <div className="restaurants-container">
-                {restaurantsList.map((restaurant) => {
-                    return <RestaurantCard resData={restaurant} />
+                {restaurantData.map((restaurant) => {
+                    return <RestaurantCard key={restaurant.info.id} resData={restaurant} />
                 })} 
             </div>
         </div>  
