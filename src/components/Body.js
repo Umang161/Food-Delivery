530d26f8a73918e734f9e02a1ab3a670a7b9cc41
Umang Agarwal <umang@Umangs-MacBook-Air.local>
2023-09-04 23:06:55 +0530

    Add Feature To Fetch Live Restaurant Data From Swiggy API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,18 @@
 import {RestaurantCard} from "./RestaurantCard"
 import {restaurantsList} from "../utils/mock-data"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { SWIGGY_LIVE_API } from "../utils/constants"
 
 export const Body = () => {
     let [restaurantData, setRestaurantData] = useState(restaurantsList);
+
+    const fetchData = async () => {
+        const liveData = await fetch(SWIGGY_LIVE_API);
+        const liveDataJSON = await liveData.json();
+        setRestaurantData(liveDataJSON?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+
+    useEffect(() => {fetchData()},[]);
 
     return(
         <div className="body">
@@ -11,8 +20,7 @@ export const Body = () => {
             <div className="filter">
                 <button className="topRated" onClick={()=>
                  {
-                  const filteredList = restaurantsList.filter((restaurant) => { return restaurant.info.avgRating > 4.5});
-                  console.log(filteredList);
+                  const filteredList = restaurantData.filter((restaurant) => { return restaurant.info.avgRating > 4.5});
                   setRestaurantData(filteredList);
                  }
                 }>Top-Rated</button>    
