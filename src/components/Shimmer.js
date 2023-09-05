6f5f9da92ec61e4305954c0dbfb0f77b699b8994
Umang Agarwal <umang@Umangs-MacBook-Air.local>
2023-09-05 22:42:38 +0530

    Add Shimmer UI Feature

---
+++ src/components/Shimmer.js
@@ -0,0 +1,23 @@
+const Shimmer = () => {
+    return(
+        <div className="shimmer_card_container">
+            <div className="shimmerCard"></div>
+            <div className="shimmerCard"></div>
+            <div className="shimmerCard"></div>
+            <div className="shimmerCard"></div>
+            <div className="shimmerCard"></div>
+            <div className="shimmerCard"></div>
+            <div className="shimmerCard"></div>
+            <div className="shimmerCard"></div>
+            <div className="shimmerCard"></div>
+            <div className="shimmerCard"></div>
+            <div className="shimmerCard"></div>
+            <div className="shimmerCard"></div>
+            <div className="shimmerCard"></div>
+            <div className="shimmerCard"></div>
+            <div className="shimmerCard"></div>
+        </div>
+    )
+}
+
+export default Shimmer;
