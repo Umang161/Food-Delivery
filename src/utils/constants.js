791fcf8894d28963b6dd36f502b98dd79c00117d
Umang Agarwal <umang@Umangs-MacBook-Air.local>
2023-09-19 20:45:14 +0530

    Change Logo

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const LOGO_LINK = "https://png.pngtree.com/template/20200610/ourmid/pngtree-food-delivery-logo-design-image_381319.jpg";
+export const LOGO_LINK ="https://www.shutterstock.com/image-illustration/delivery-scooter-cartoon-600w-1121572010.jpg";
 export const CDN_LINK="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const SWIGGY_LIVE_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 export const MENU_LINK = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=";
